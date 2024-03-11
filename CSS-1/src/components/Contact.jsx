a028afb12a6058f67125a46ae1f791fa296634b6
banothshiva <banothshiva9052@gmail.com>
2024-03-11 21:51:05 +0530

    REACT FIRST  CSS APPLICATION

---
+++ CSS-1/src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Home from "./Home";
+const Contact=()=>{
+    return(<>
+    <div id="h1">
+     STUDENT NAME:Banoth Shiva <br></br>
+      STUDENT FEES:25000        <br></br>
+     STUDENT EMAIL:banothshiva9052@gmail.com <br></br>
+  
+    </div>
+    <div className="class1">
+        COMPANY NAME :Google        <br></br>
+        ROLE:Associate Softwere Engineer        <br></br>
+        LOCATION :Hyderabad        <br></br>
+    </div>
+    
+    
+    </>)
+}
+export default Contact;
---
+++ CSS-1/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Home.css';
+const Home = ()=>{
+   return(
+   <>
+   <body id="b">
+   <h1 id="h1">WELL COME TO THE HOME PAGE </h1>
+   <h3 id="h1">ABOUT</h3>
+   <p className="class1">Cascading Style Sheets (CSS) is a style sheet
+    language used for specifying the presentation 
+    and styling of a document written in a markup language such
+     as HTML or XML (including XML dialects such as SVG,
+      MathML or XHTML). CSS is a cornerstone technology of the 
+      World Wide Web, alongside HTML and JavaScript.</p>
+      </body>
+   </>) 
+}
+export default  Home;
